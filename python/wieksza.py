e918973b7d82faf9ae8ab5a7752d1826624c2e22
modpat135 <patrycja.modras@lo1.sandomierz.pl>
2019-10-31 12:34:40 +0100

    schematy blokowe początek

---
+++ python/wieksza.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+def main(args):
+    # a = 10
+    a = input('podaj liczbę: ')
+    # b = 5
+    b = input('podaj liczbę: ')
+    print('Suma:', a+b)
+    if a > b
+        print (a)
+    else:
+        print (b)
+        
+    print(a + b)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
