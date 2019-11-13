bc1246dceb91424a5a56f8a6b0b7bd9e9bbbf3f2
modpat135 <patrycja.modras@lo1.sandomierz.pl>
2019-11-13 11:43:50 +0100

    pętla for

--- python/petla.py
+++ python/petla.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 
+def sumuj_parzyste():
+    suma = 0
+    for i in range(0, 101, 2):
+        #liczba = int(input('podaj liczbę: '))
+        suma = suma + i
+    print(suma)
 
 def main(args):
-    suma = 0
-    for i in range(3):
-        liczba = int(input('podaj liczbę: '))
-        suma = suma + liczba
-    print(suma)
+    sumuj_parzyste()
         
     return 0
 
