614ea81895a6072d1a6d9440b73798fafafb7c1f
modpat135 <patrycja.modras@lo1.sandomierz.pl>
2020-01-23 12:36:29 +0100

    kalkulator znowu

--- python/kalkulator.py
+++ python/kalkulator.py
@@ -3,13 +3,47 @@
 #
 #  kalkulator.py
 
+wynik = 0
+
+
+def pobierzliczbe():
+    liczba1 = input('Podaj liczbę: ')
+    return liczba1
+
+def dodaj(liczba1 + liczba2):
+    global wynik
+    wynik = liczba1 + liczba2
+
+def odejmij():
+    wynik= liczba1 - liczba2
+    pass
+    
+def pomnoz():
+    wynik= liczba1 * liczba2
+    pass
+    
+def podziel():
+    wynik= liczba1 / liczba2
+    pass
 
 
 def main(args):
-    a = input('Podaj działanie które chcesz wykonać(+,-,*,/): ')
+    operacja = input('Podaj działanie które chcesz wykonać(+,-,*,/): ')
+    liczba1 = pobierzliczbe()
+    liczba2 = pobierzliczbe()
     
-    dodaj = (   
+    if operacja == '+':
+        dodaj(liczba1, liczba2)
+    elif operacja == '-':
+        odejmij(liczba1, liczba2)
+    elif operacja == '*':
+        pomnoz(liczba1, liczba2)
+    elif operacja == '/':
+        podziel(liczba1, liczba2)
+    else:
+        print ('coś jest nie tak')
     
+    print (wynik)
     return 0
 
 if __name__ == '__main__':
