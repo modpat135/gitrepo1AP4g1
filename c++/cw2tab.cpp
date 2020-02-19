b7680494a978117f03f4f461fc19cfb27aa235d6
modpat135 <patrycja.modras@lo1.sandomierz.pl>
2020-02-19 11:39:58 +0100

    dokończyć

--- c++/cw2tab.cpp
+++ c++/cw2tab.cpp
@@ -4,23 +4,42 @@
 
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 void wypelnij(int t[][10], int w, int k, int n) {
     srand(time(NULL));
-    cout << rand() % (n+1) << endl;
+    for(int i=0; i<w; i++) {
+        for(int j=0; j<k; j++) {
+            t[i][j] = rand() % (n+1);
+            cout << i << "," << j << " " << t[i][j] << endl;
+        }
     }
+    
+}
+
+void sumujw(int t[][10], int w, int k) {
+    int sumaw = 0;
+    for(int i=0; i<w; i++) {
+        for(int j=0; j<k; j++) {
+            cout << setw(4) << t[i][j];
+            sumaw += t[i][j];
+        }
+        cout << setw(6) << sumaw <<endl;
+    }
+}
 
 int main(int argc, char **argv)
 {
 	int n = 0;
-    //cout << "Podaj wartość maksymalną: ";
-    //cin >> n;
+    cout << "Podaj wartość maksymalną: ";
+    cin >> n;
     
     int w = 5;
     int k = 10;
     int tab[w][10];
-    wypelnij(tab, w, k, 100);
+    wypelnij(tab, w, k, n);
+    sumujw( tab, w, k);
     
     
 	return 0;
